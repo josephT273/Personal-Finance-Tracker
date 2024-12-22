b6da849efa0fcf8a13d7264400b41c0720b4d32c
Josepht273 <Josephtadessem@gmail.com>
2024-12-22 19:48:42 +0300

    Create Boiler plate code

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+struct Daa
+
+class CSVFunction
+{
+public:
+	void createFile(string fileName);
+	void readFile(string fileName);
+	void saveData();
+	void updateData();
+	void deleteDocument();
+};
+class Expence
+{
+	int salary;
+	string username;
+	string password;
+
+public:
+	void login();
+	void saveToFile();
+	void readFromFile();
+	void display();
+	void calculateExpence();
+};
+
+int main()
+{
+
+	return 0;
+}
