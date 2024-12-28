b358d4483497b562f151d149096d7f36cc1ff56e
josepht273 <josephtadessem@gmail.com>
2024-12-28 11:51:39 +0300

    Add menu for the app

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
-#include <openssl/sha.h>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using namespace std;
 
 #define TRANSACTIONS "transactions.csv"
 #define USERS "users.csv"
 string gUserName;
-
-using namespace std;
 
 struct UserInfo
 {
@@ -13,67 +16,51 @@ struct UserInfo
 	string password;
 };
 
-struct ExpenceInfo
+struct ExpenseInfo
 {
 	string username;
 	string type;
 	double amount;
 	string description;
 };
-class PersonalExpenceTracker
+
+class PersonalExpenseTracker
 {
 public:
 	void createFile();
 	void readFile();
-	void saveData(ExpenceInfo);
-	void updateData(ExpenceInfo);
-	void deleteDocument(ExpenceInfo);
+	void saveData(ExpenseInfo);
+	void updateData(ExpenseInfo);
+	void deleteDocument(ExpenseInfo);
 	bool registerUser(UserInfo);
 	bool loginUser(UserInfo);
-	string hashPassword(string);
-};
-class Expence
-{
-	int salary;
-	string username;
-	string password;
+	void run();
 
-public:
-	void login();
-	void saveToFile();
-	void readFromFile();
-	void display();
-	void calculateExpence();
+private:
+	bool fileExists(const string &fileName);
 };
 
-string PersonalExpenceTracker::hashPassword(string password)
+bool PersonalExpenseTracker::fileExists(const string &fileName)
 {
-	unsigned char hash[300];
-	SHA256((unsigned char *)password.c_str(), password.size(), hash);
-	stringstream ss;
-	for (int i = 0; i < SHA256_DIGEST_LENGTH; i++)
-	{
-		ss << hex << setw(2) << setfill('0') << (int)hash[i];
-	}
-
-	retrun ss.str();
+	ifstream file(fileName);
+	return file.good();
 }
 
-bool PersonalExpenceTracker::registerUser(UserInfo userInfo)
+bool PersonalExpenseTracker::registerUser(UserInfo userInfo)
 {
-	ifstream file(USERS);
+	ofstream file(USERS, ios::app);
 	if (!file)
 	{
-		cerr << "Error oening users.csv!" << endl;
+		cerr << "Error opening users.csv!" << endl;
 		return false;
 	}
 
-	file << userInfo.username << "," << PersonalExpenceTracker::hashPassword(userInfo.password) << endl;
+	file << userInfo.username << "," << userInfo.password << endl;
 	file.close();
 	return true;
 }
 
-bool PersonalExpenceTracker::loginUser(UserInfo userInfo)
+bool PersonalExpenseTracker::loginUser(UserInfo userInfo)
 {
 	ifstream file(USERS);
 	if (!file)
@@ -88,7 +75,7 @@ bool PersonalExpenceTracker::loginUser(UserInfo userInfo)
 		stringstream ss(line);
 		getline(ss, user, ',');
 		getline(ss, pass, ',');
-		if (user == userInfo.username && pass == PersonalExpenceTracker::hashPassword(userInfo.password))
+		if (user == userInfo.username && pass == userInfo.password)
 		{
 			gUserName = user;
 			return true;
@@ -97,37 +84,29 @@ bool PersonalExpenceTracker::loginUser(UserInfo userInfo)
 	return false;
 }
 
-void PersonalExpenceTracker::createFile()
+void PersonalExpenseTracker::createFile()
 {
 	if (!fileExists(USERS))
 	{
 		ofstream usersFile(USERS);
-		if (usersFile(USERS))
-		{
-			cout << "Created users file: " << USERS << endl;
-		}
-
+		cout << "Created users file: " << USERS << endl;
 		usersFile.close();
 	}
 
 	if (!fileExists(TRANSACTIONS))
 	{
 		ofstream transactionFile(TRANSACTIONS);
-		if (transactionFile(TRANSACTIONS))
-		{
-			cout << "Created users file: " << TRANSACTIONS << endl;
-		}
-
+		cout << "Created transactions file: " << TRANSACTIONS << endl;
 		transactionFile.close();
 	}
 }
 
-void PersonalExpenceTracker::readFile()
+void PersonalExpenseTracker::readFile()
 {
 	ifstream file(TRANSACTIONS);
 	if (!file)
 	{
-		cerr << "Error opening transaction.csv" << endl;
+		cerr << "Error opening transactions.csv" << endl;
 		return;
 	}
 
@@ -139,7 +118,9 @@ void PersonalExpenceTracker::readFile()
 		getline(ss, user, ',');
 		getline(ss, type, ',');
 		ss >> amount;
+		ss.ignore(); // Ignore comma
 		getline(ss, description, ',');
+
 		if (user != gUserName)
 		{
 			continue;
@@ -148,28 +129,232 @@ void PersonalExpenceTracker::readFile()
 	}
 }
 
-void PersonalExpenceTracker::saveData(ExpenceInfo expenceInfo)
+void PersonalExpenseTracker::saveData(ExpenseInfo expenseInfo)
 {
 	ofstream file(TRANSACTIONS, ios::app);
 	if (!file)
 	{
-		cerr << "Error opening transaction.csv!" << endl;
+		cerr << "Error opening transactions.csv!" << endl;
 		return;
 	}
 
-	file << expenceInfo.username << "," << expenceInfo.type << "," << expenceInfo.amount << "," << expenceInfo.description << endl;
+	file << expenseInfo.username << "," << expenseInfo.type << "," << expenseInfo.amount << "," << expenseInfo.description << endl;
 	file.close();
 }
 
-void PersonalExpenceTracker::updateData(ExpenceInfo expenceInfo)
+void PersonalExpenseTracker::updateData(ExpenseInfo expenseInfo)
 {
+	ifstream file(TRANSACTIONS);
+	if (!file)
+	{
+		cerr << "Error opening transactions.csv!" << endl;
+		return;
+	}
+
+	vector<string> lines;
+	string line;
+	while (getline(file, line))
+	{
+		lines.push_back(line);
+	}
+	file.close();
+
+	ofstream outFile(TRANSACTIONS);
+	if (!outFile)
+	{
+		cerr << "Error opening transactions.csv!" << endl;
+		return;
+	}
+
+	for (const string &line : lines)
+	{
+		stringstream ss(line);
+		string user, type, description;
+		double amount;
+		getline(ss, user, ',');
+		getline(ss, type, ',');
+		ss >> amount;
+		ss.ignore(); // Ignore comma
+		getline(ss, description, ',');
+
+		if (user == expenseInfo.username && type == expenseInfo.type && amount == expenseInfo.amount && description == expenseInfo.description)
+		{
+			outFile << expenseInfo.username << "," << expenseInfo.type << "," << expenseInfo.amount << "," << expenseInfo.description << endl;
+		}
+		else
+		{
+			outFile << line << endl;
+		}
+	}
+	outFile.close();
 }
 
-void PersonalExpenceTracker::deleteDocument(ExpenceInfo expenceInfo)
+void PersonalExpenseTracker::deleteDocument(ExpenseInfo expenseInfo)
 {
+	ifstream file(TRANSACTIONS);
+	if (!file)
+	{
+		cerr << "Error opening transactions.csv!" << endl;
+		return;
+	}
+
+	vector<string> lines;
+	string line;
+	while (getline(file, line))
+	{
+		lines.push_back(line);
+	}
+	file.close();
+
+	ofstream outFile(TRANSACTIONS);
+	if (!outFile)
+	{
+		cerr << "Error opening transactions.csv!" << endl;
+		return;
+	}
+
+	for (const string &line : lines)
+	{
+		stringstream ss(line);
+		string user, type, description;
+		double amount;
+		getline(ss, user, ',');
+		getline(ss, type, ',');
+		ss >> amount;
+		ss.ignore(); // Ignore comma
+		getline(ss, description, ',');
+
+		if (user == expenseInfo.username && type == expenseInfo.type && amount == expenseInfo.amount && description == expenseInfo.description)
+		{
+			continue;
+		}
+		outFile << line << endl;
+	}
+	outFile.close();
 }
+
+ExpenseInfo parseExpenseInfo()
+{
+	ExpenseInfo expenseInfo;
+
+	cout << "Enter type of expense: ";
+	getline(cin, expenseInfo.type);
+	cout << "Enter amount: ";
+	cin >> expenseInfo.amount;
+	cin.ignore();
+	cout << "Enter description: ";
+	getline(cin, expenseInfo.description);
+	expenseInfo.username = gUserName;
+
+	return expenseInfo;
+}
+
+void PersonalExpenseTracker::run()
+{
+	createFile();
+	int choice;
+	do
+	{
+		cout << "\nWelcome to Personal Expense Tracker\n";
+		cout << "1. Login\n";
+		cout << "2. Register\n";
+		cout << "3. Exit\n";
+		cout << "Enter your choice: ";
+		cin >> choice;
+
+		UserInfo userInfo;
+
+		switch (choice)
+		{
+		case 1:
+			cout << "Enter username: ";
+			cin >> userInfo.username;
+			cout << "Enter password: ";
+			cin >> userInfo.password;
+
+			if (loginUser(userInfo))
+			{
+				cout << "Login successful! Welcome, " << gUserName << "!\n";
+				break;
+			}
+			else
+			{
+				cout << "Invalid username or password! Please try again.\n";
+			}
+			continue; // Return to the main menu after login failure.
+
+		case 2:
+			cout << "Enter a new username: ";
+			cin >> userInfo.username;
+			cout << "Enter a new password: ";
+			cin >> userInfo.password;
+
+			if (registerUser(userInfo))
+			{
+				cout << "Registration successful! You can now log in.\n";
+			}
+			else
+			{
+				cout << "Error registering user. Please try again.\n";
+			}
+			continue; // Return to the main menu after registration.
+
+		case 3:
+			cout << "Exiting the application. Goodbye!\n";
+			return;
+
+		default:
+			cout << "Invalid choice! Please try again.\n";
+		}
+	} while (choice != 1);
+
+	// Main application menu after successful login.
+	do
+	{
+		cout << "\nMenu:\n";
+		cout << "1. Add Expense\n";
+		cout << "2. Edit Expense\n";
+		cout << "3. Read Expenses\n";
+		cout << "4. Delete Expense\n";
+		cout << "5. Exit\n";
+		cout << "Enter your choice: ";
+		cin >> choice;
+		cin.ignore();
+
+		switch (choice)
+		{
+		case 1:
+			ExpenseInfo expenseInfo = parseExpenseInfo();
+			saveData(expenseInfo);
+			break;
+
+		case 2:
+			ExpenseInfo expenseInfo = parseExpenseInfo();
+			updateData(expenseInfo);
+			break;
+
+		case 3:
+			readFile();
+			break;
+
+		case 4:
+			ExpenseInfo expenseInfo = parseExpenseInfo();
+			deleteDocument(expenseInfo);
+			break;
+
+		case 5:
+			cout << "Exiting...\n";
+			break;
+
+		default:
+			cout << "Invalid choice! Please try again.\n";
+		}
+	} while (choice != 5);
+}
+
 int main()
 {
-
+	PersonalExpenseTracker pet;
+	pet.run();
 	return 0;
 }
